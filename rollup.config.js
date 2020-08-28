fd8f41e6e351f1020d5c32e0b09b8fa3f420ab52
Saitama Sama <gourabnag12@gmail.com>
2020-08-29 02:08:52 +0530

    init

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import html from '@rollup/plugin-html';
+import scss from 'rollup-plugin-scss';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import typescript from '@rollup/plugin-typescript';
+
+let isProd = false;
+if (process.env.NODE_ENV === 'production') isProd = true;
+const extensions = ['.js', '.ts', '.tsx'];
+
+export default {
+  input: join("src", "index.tsx"),
+  output: {
+    file: join("public", "bundle.js"),
+    format: 'iife',
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+    }),
+    resolve({
+      extensions,
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    typescript(),
+    babel({
+      extensions,
+      exclude: /node_modules/,
+      babelrc: false,
+      babelHelpers: "runtime",
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        'react-require',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-object-rest-spread', {
+          useBuiltIns: true,
+        }],
+        ['@babel/plugin-transform-runtime', {
+          corejs: 3,
+          helpers: true,
+          regenerator: true,
+          useESModules: false,
+        }],
+      ],
+    }),
+    html({
+      fileName: 'index.html',
+      title: 'Commie',
+      template: ({ title }) => {
+        const file = readFileSync(join("src", "index.html"));
+        return file.toString().replace(/\{\{ title \}\}/g, title);
+      },
+    }),
+    scss({
+      output: 'public/index.css',
+    }),
+    (isProd && terser()),
+    (!isProd && serve({
+      host: 'localhost',
+      port: 3000,
+      open: false,
+      contentBase: ['public'],
+    })),
+    (!isProd && livereload({
+      watch: 'public',
+    })),
+  ],
+};
---
+++ src/modules/splash/components/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Index extends React.Component<Record<string, unknown>> {
+  public render() {
+    return (
+      <main className="splash-page">
+        //
+      </main>
+    )
+  }
+}
