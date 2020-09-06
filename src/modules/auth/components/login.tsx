d4539f1883cdb93ca67ed26a49cefba0d6fdc77e
Saitama Sama <gourabnag12@gmail.com>
2020-09-06 18:57:17 +0530

    misc: create login file

---
+++ src/modules/auth/components/login.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Login: React.FunctionComponent<Record<string, unknown>> = (): JSX.Element => {
+  return (
+    <div className="login-page"></div>
+  );
+};
