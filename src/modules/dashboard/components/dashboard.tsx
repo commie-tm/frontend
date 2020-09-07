6b1d7ec16e595fe1a08443abc71192cdd302962b
Saitama Sama <gourabnag12@gmail.com>
2020-09-07 23:43:18 +0530

    minor: create dashboard page

---
+++ src/modules/dashboard/components/dashboard.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Dashboard: React.FunctionComponent<Record<string, unknown>> = (): JSX.Element => {
+  return (
+    <div className="dashboard-page"></div>
+  );
+}
